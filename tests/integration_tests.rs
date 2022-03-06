16ff9940c930c466f46816aa26543bd8f0e0a110
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2022-03-06 18:24:24 +0100

    Call our main program successfully

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,12 +1,14 @@
+use runfiles::Runfiles;
 use std::process::Command;
 
 #[test]
-fn test_succeeds_with_empty_bazel_output() {
-    let output = Command::new("sh")
-        .arg("-c")
-        .arg("echo hello")
+fn test_succeeds_with_empty_stdin() {
+    let runfiles = Runfiles::create().unwrap();
+    let program = runfiles.rlocation("__main__/src/main");
+
+    let output = Command::new(program)
         .output()
-        .expect("Something went wrong");
+        .expect("Running the command failed");
 
     assert_eq!(String::from_utf8(output.stdout).unwrap(), "hello\n")
 }
