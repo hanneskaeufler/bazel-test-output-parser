60bd3d266d4ff78453f75a207148fe63cfb43bcb
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2022-03-06 18:06:55 +0100

    Run an integration test on a shell cmd

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,2 +1,12 @@
+use std::process::Command;
+
 #[test]
-fn test_succeeds_with_empty_bazel_output() {}
+fn test_succeeds_with_empty_bazel_output() {
+    let output = Command::new("sh")
+        .arg("-c")
+        .arg("echo hello")
+        .output()
+        .expect("Something went wrong");
+
+    assert_eq!(String::from_utf8(output.stdout).unwrap(), "hello\n")
+}
