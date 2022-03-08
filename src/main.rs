f4ea67600d833b2981e60f36b87dc67a8c8bc39b
Hannes Käufler <hanneskaeufler@users.noreply.github.com>
2022-03-08 22:02:26 +0100

    Hk help (#2)
    * Apply clippy suggestion that wasn't automatic
    
    * Show small help output when passing --help
    
    * Remove useless interpolation

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
+use std::env;
 use std::io;
 use std::io::Read;
 
 fn main() -> Result<(), io::Error> {
-    let mut buffer = String::new();
+    let args: Vec<String> = env::args().collect();
 
+    if args.len() > 1 && args[1] == "--help" {
+        println!(
+            "\
+This is a stupid tiny parser for the output
+produced by bazel's (https://bazel.build) test runner.
+When running e.g. `bazel test //...`, a log file will
+pre written and printed to stdout, which can be parsed
+by this program to get a list of junit test results that
+bazel produced.
+
+Usage example:
+    cat my.log | bazel-output-parser"
+        );
+        return Ok(());
+    }
+
+    let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
 
     if buffer.is_empty() {
